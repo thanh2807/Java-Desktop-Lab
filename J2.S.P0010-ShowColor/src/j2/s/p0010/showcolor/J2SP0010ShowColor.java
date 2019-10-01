310a57b6190d70add7c1f47bcc47a714e3b1acc4
thanh2807 <55226962+thanh2807@users.noreply.github.com>
2019-10-01 14:49:38 +0700

    Add files via upload

---
+++ J2.S.P0010-ShowColor/src/j2/s/p0010/showcolor/J2SP0010ShowColor.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j2.s.p0010.showcolor;
+
+/**
+ *
+ * @author Nhat Thanh
+ */
+public class J2SP0010ShowColor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
